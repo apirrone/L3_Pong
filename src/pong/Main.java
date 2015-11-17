857d712983b9443d56551e0feb2e860f56031657
antoine <antoine@localhost.localdomain>
2015-11-17 15:44:00 +0100

    modification

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -1,6 +1,11 @@
 package pong;
 
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import network.Client;
+import network.Server;
 import gui.Window;
 import gui.Pong;
 
@@ -9,6 +14,17 @@ import gui.Pong;
  */
 public class Main  {
 	public static void main(String[] args) {
+		
+		if(args.length == 0){//Serveur
+			Server server = new Server(2009);
+			server.waitForConnection();
+		}
+		else{//client
+			Client client = new Client();
+			String addr = args[0];
+			client.connect(addr, 2009);
+		}
+		
 		Pong pong = new Pong();
 		Window window = new Window(pong);
 		window.displayOnscreen();
