5b69458b5a746a5e13ea816664d195f37bdae926
Orebuc <pierre.cuberocastan@gmail.com>
2016-01-02 17:00:41 +0100

    Implémentation des fonctions de MagneticRacket

--- src/gui/MagneticRacket.java
+++ src/gui/MagneticRacket.java
@@ -12,42 +12,37 @@ public class MagneticRacket extends RacketType {
 	/**
 	 * "moveBall" change avec une "magneticRacket", A terminer
 	 */
-	public void moveBallOnRacketCote(int size_pong_x, int size_pong_y, RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
-				if (racketPlayer.itemOnRacketCote(ball)) {
-					ball.setSpeed(racketPlayer.position.x,racketPlayer.speed);
+	public void moveBallOnRacketCote(RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
+		if (racketPlayer.itemOnRacketCote(ball) || racketOpponent.itemOnRacketCote(ball)) {
+			// Valeur calculee par : ((distance entre le milieu de la balle et le milieu de la raquette) / 
+			// ((1/2)* (longueur de la raquette + longueur de la balle))) * 
+			// decalage Maximal sur Y
+			float valueY = ((((float)ball.getPosition().y + (ball.getHeight() / 2)) - (racketPlayer.getPosition().y + (racketPlayer.getHeight() / 2))) / 
+					(((racketPlayer.getHeight() + ball.getHeight()) / 2))) * Pong.DECALAGE_MAX_ON_RACKET;
+			// Si la balle a un effet
+			if(ball.getHasLift()){
+				// On ajoute l'effet a la direction de la balle, cet effet est defini comme un pourcentage
+				if(valueY > 0)
+					valueY = (float)valueY + ((valueY * ball.getLiftSpeed())/100);
+				else{
+					if(valueY < 0)
+						valueY = (float)valueY + ((valueY * (-ball.getLiftSpeed()))/100);
+					else
+						valueY = 0;
 				}
-				if (racketOpponent.itemOnRacketCote(ball)) {
-					ball.setSpeed(racketOpponent.position.x,racketOpponent.speed);
-				}
-				//ball.position.translate(ball.getSpeed().x/Math.abs(ball.getSpeed().x), 0);
+				// On enleve l'effet sur la balle
+				ball.setHasLift(false);
+			}
+			ball.setSpeedY(Math.round(valueY));
+			ball.setSpeedX(0);
+		}
 	}
 	
-	public void moveBallOnRacketOther(int size_pong_x, int size_pong_y, RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
-		if (racketPlayer.itemOnRacketHaut(ball)) {
-			ball.setSpeed(racketPlayer.position.x,racketPlayer.speed);
-		}
-		 if (racketOpponent.itemOnRacketHaut(ball)) {
-			ball.setSpeed(racketPlayer.position.x,racketOpponent.speed);
-		}
-		if (racketPlayer.itemOnRacketCorner(ball)) {
-			ball.setSpeed(racketPlayer.position.x,racketPlayer.speed);
-		}
-		if (racketOpponent.itemOnRacketCorner(ball)) {
-			ball.setSpeed(racketPlayer.position.x,racketOpponent.speed);
-		}
-		//ball.position.translate(0, ball.getSpeed().y/Math.abs(ball.getSpeed().y));
-	}
-
-	@Override
-	void moveBallOnRacketCote(RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	void moveBallOnRacketOther(RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
-		// TODO Auto-generated method stub
-		
+	public void moveBallOnRacketOther(RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
+		if (racketPlayer.itemOnRacketHaut(ball) || racketOpponent.itemOnRacketHaut(ball))
+			ball.setSpeed(0, -ball.getSpeed().y);	
+		if (racketPlayer.itemOnRacketCorner(ball) || racketOpponent.itemOnRacketCorner(ball))
+			ball.setSpeed(0, -ball.getSpeed().y);
 	}
 
 	@Override
--- src/gui/Racket.java
+++ src/gui/Racket.java
@@ -70,35 +70,12 @@ public class Racket extends RacketType{
 	 * Reaction de la balle si elle touche le cote de la raquette
 	 */
 	public void moveBallOnRacketCote(RacketType racketPlayer, RacketType racketOpponent, BallType ball) {
-		if (racketPlayer.itemOnRacketCote(ball)) {
+		if (racketPlayer.itemOnRacketCote(ball) || racketOpponent.itemOnRacketCote(ball)) {
 			// Valeur calculee par : ((distance entre le milieu de la balle et le milieu de la raquette) / 
 			// ((1/2)* (longueur de la raquette + longueur de la balle))) * 
 			// decalage Maximal sur Y
 			float valueY = ((((float)ball.getPosition().y + (ball.getHeight() / 2)) - (racketPlayer.getPosition().y + (racketPlayer.getHeight() / 2))) / 
 					(((racketPlayer.getHeight() + ball.getHeight()) / 2))) * Pong.DECALAGE_MAX_ON_RACKET;
-			// Si la balle a un effet
-			if(ball.getHasLift()){
-				// On ajoute l'effet a la direction de la balle, cet effet est defini comme un pourcentage
-				if(valueY > 0)
-					valueY = (float)valueY + ((valueY * ball.getLiftSpeed())/100);
-				else{
-					if(valueY < 0)
-						valueY = (float)valueY + ((valueY * (-ball.getLiftSpeed()))/100);
-					else
-						valueY = 0;
-				}
-				// On enleve l'effet sur la balle
-				ball.setHasLift(false);
-			}
-			ball.setSpeedY(Math.round(valueY));
-			ball.setSpeedX(-ball.getSpeed().x);
-		}
-		if (racketOpponent.itemOnRacketCote(ball)) {
-			// Valeur calculee par : ((distance entre le milieu de la balle et le milieu de la raquette) / 
-			// ((1/2)* (longueur de la raquette + longueur de la balle))) * 
-			// decalage Maximal sur Y
-			float valueY = ((((float)ball.getPosition().y + (ball.getHeight() / 2)) - (racketOpponent.getPosition().y + (racketOpponent.getHeight() / 2))) / 
-					(((racketOpponent.getHeight() + ball.getHeight()) / 2))) * Pong.DECALAGE_MAX_ON_RACKET;
 			// Si la balle a un effet
 			if(ball.getHasLift()){
 				// On ajoute l'effet a la direction de la balle, cet effet est defini comme un pourcentage
