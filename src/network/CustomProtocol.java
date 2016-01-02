c86362757672ddd047a7e016fdb5a4ff088fb8e8
antoine <antoine@localhost.localdomain>
2016-01-02 14:18:14 +0100

    Debut changement de la facon dont les données transitent sur le reseau, changement de string vers tableau d'octets --> plus léger

--- src/network/CustomProtocol.java
+++ src/network/CustomProtocol.java
@@ -20,6 +20,9 @@ public class CustomProtocol {
 		this.timeToRand = 0;
 	}
 	
+	/** 
+	 * Fonction d'ajout des variables au CustomProtocol
+	 */
 	public void setCustomProtocol(int yRacket, Point ballPosition, boolean haslift, int liftSpeed, int ballSpeedY, long timeToRand) {
 		this.yRacket = yRacket;
 		this.ballPosition.setLocation(ballPosition);
@@ -30,11 +33,35 @@ public class CustomProtocol {
 	}
 	
 	/** 
-	 * Fonction d'ajout des variables au CustomProtocol
+	 * Fonction de conversion d'une string en CustomProtocol
 	 */
 	public void setCustomProtocol(String s) {
 		if(!s.isEmpty() && !s.equals("error")) {
 			// Separe dans un tableau de String les elements de la chaine "s" separes par ";"
+			String[] st = s.split(";");
+			this.yRacket = Integer.parseInt(st[0]);
+			this.ballPosition.setLocation(Integer.parseInt(st[1]), Integer.parseInt(st[2]));
+			this.hasLift = Boolean.valueOf(st[3]);
+			this.liftSpeed = Integer.parseInt(st[4]);
+			this.ballSpeedY = Integer.parseInt(st[5]);
+			this.timeToRand = Long.parseLong(st[6]);
+		} else {
+			this.yRacket = 0;
+			this.ballPosition.setLocation(0,0);
+			this.hasLift = false;
+			this.liftSpeed = 0;
+			this.ballSpeedY = 0;
+			this.timeToRand = 0;
+		}
+	}
+	
+	/** 
+	 * Fonction de conversion d'une string en CustomProtocol
+	 */
+	public void setCustomProtocol(byte[] b) {
+		if(b.length != 0 && !b.toString().equals("error")) {
+			// Separe dans un tableau de String les elements de la chaine "s" separes par ";"
+			String s = b.toString();
 			String[] st = s.split(";");
 			this.yRacket = Integer.parseInt(st[0]);
 			this.ballPosition.setLocation(Integer.parseInt(st[1]), Integer.parseInt(st[2]));
@@ -74,6 +101,17 @@ public class CustomProtocol {
 		return s;
 	}
 
+
+	public byte[] toByteArray(){
+		byte[] ret;
+		
+		ret = this.toString().getBytes();
+		
+		return ret;
+	}
+	
+	
+	
 	/**
 	 * Get / Set accesseurs des attributs
 	 */
