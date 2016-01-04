294dc78c2172240516cf0375f7b85554d0a4dc3a
Orebuc <pierre.cuberocastan@gmail.com>
2016-01-04 23:03:16 +0100

    100eme commits! FELICITATION !!!! Vous en ètes enfin arrivés. Que dire de plus? Rien l'exploit est atteint. C'est un grand pas pour l'Homme, mais un très grand pas pour l'Homme

--- src/gui/Ball.java
+++ src/gui/Ball.java
@@ -3,7 +3,7 @@ package gui;
 import java.awt.Toolkit;
 
 public class Ball extends BallType {
-	
+
 	public Ball(boolean serveur) {
 		super(Toolkit.getDefaultToolkit().createImage(
 				ClassLoader.getSystemResource("ressource/ball.png")) ,serveur);
